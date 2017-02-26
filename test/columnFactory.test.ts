70b9b9ff216669b41fed1ab215da816c83321754
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2017-02-26 21:49:04 +0200

    Add travic yaml.

---
+++ test/columnFactory.test.ts
@@ -0,0 +1,20 @@
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+import { ColumnFactory, Column, ColumnPositioning } from '../src/column';
+
+suite("ColumnFactory Tests", () => {
+/*
+    test("generateColumns() sets cell values", () => {
+        const rows = [
+            ["one", "two", "three"],
+            ["-|-", "-|-", "---"]
+        ];
+        const columns = ColumnFactory.generateColumns(rows);
+        
+        assert.equal(rawColumn.columnValues, values);
+    });*/
+});
+
