0bed7931c2b1f76d8f80be8f21d6fb89cd9b7638
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2017-02-27 19:19:06 +0200

    Finish up the column factory tests.

--- test/columnFactory.test.ts
+++ test/columnFactory.test.ts
@@ -1,20 +1,117 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 import { ColumnFactory, Column, ColumnPositioning } from '../src/column';
 
 suite("ColumnFactory Tests", () => {
 
-    test("generateColumns() one row and separator creates 3 columns", () => {
+    test("generateColumns() array with 1 element creates 1 columns", () => {
         const rows = [
-            ["one", "two", "three"],
-            ["-|", "-|", "-"]
+            ["one"]
+        ];
+        const columns = Array.from(ColumnFactory.generateColumns(rows));
+        
+        assert.equal(columns.length, 1);
+    });
+
+    test("generateColumns() array with 3 elements creates 3 columns", () => {
+        const rows = [
+            ["one", "two", "three"]
         ];
         const columns = Array.from(ColumnFactory.generateColumns(rows));
         
         assert.equal(columns.length, 3);
+    });
+
+    test("generateColumns() 2D array with 4 elements creates 4 columns", () => {
+        const rows = [
+            ["one", "two", "three", "four"],
+            ["1", "2", "3", "4"]
+        ];
+        const columns = Array.from(ColumnFactory.generateColumns(rows));
+        
+        assert.equal(columns.length, 4);
+    });
+
+    test("generateColumns() set correct ColumnPositioning", () => {
+        const rows = [
+            ["one", "two", "three", "four"],
+            ["1", "2", "3", "4"]
+        ];
+        const columns: Column[] = Array.from(ColumnFactory.generateColumns(rows));
+        assert.equal(columns.length, 4);
+        assert.equal(columns[0].getPositioning(), ColumnPositioning.First);
+        assert.equal(columns[1].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[2].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[3].getPositioning(), ColumnPositioning.Last);
+    });
+
+    test("generateColumns() adds empty last column if first column is empty", () => {
+        const rows = [
+            ["", "two", "three", "four"],
+            ["", "2", "3", "4"]
+        ];
+        const columns: Column[] = Array.from(ColumnFactory.generateColumns(rows));
+        assert.equal(columns.length, 5);
+
+        assert.equal(columns[0].getPositioning(), ColumnPositioning.First);
+        assert.equal(columns[0].isEmpty(), true);
+        assert.equal(columns[1].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[2].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[3].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[4].getPositioning(), ColumnPositioning.Last);
+        assert.equal(columns[4].isEmpty(), true);
+    });
+
+    test("generateColumns() removes last empty column if first is not empty", () => {
+        const rows = [
+            ["one", "two", "three", "four", ""],
+            ["1", "2", "3", "4", ""]
+        ];
+        const columns: Column[] = Array.from(ColumnFactory.generateColumns(rows));
+        assert.equal(columns.length, 4);
+
+        assert.equal(columns[0].getPositioning(), ColumnPositioning.First);
+        assert.equal(columns[0].isEmpty(), false);
+        assert.equal(columns[1].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[2].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[3].getPositioning(), ColumnPositioning.Last);
+        assert.equal(columns[3].isEmpty(), false);
+    });
+
+    test("generateColumns() does not touch first or last columns if they're both empty", () => {
+        const rows = [
+            ["", "one", "two", "three", "four", ""],
+            ["", "1", "2", "3", "4", ""]
+        ];
+        const columns: Column[] = Array.from(ColumnFactory.generateColumns(rows));
+        assert.equal(columns.length, 6);
+
+        assert.equal(columns[0].getPositioning(), ColumnPositioning.First);
+        assert.equal(columns[0].isEmpty(), true);
+        assert.equal(columns[1].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[2].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[3].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[4].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[5].getPositioning(), ColumnPositioning.Last);
+        assert.equal(columns[5].isEmpty(), true);
+    });
+
+    test("generateColumns() keeps any middle empty columns", () => {
+        const rows = [
+            ["one", "", "two", "three", "", "four"],
+            ["1", "", "2", "3", "", "4"]
+        ];
+        const columns: Column[] = Array.from(ColumnFactory.generateColumns(rows));
+        assert.equal(columns.length, 6);
+
+        assert.equal(columns[0].getPositioning(), ColumnPositioning.First);
+        assert.equal(columns[1].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[1].isEmpty(), true);
+        assert.equal(columns[2].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[3].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[4].getPositioning(), ColumnPositioning.Middle);
+        assert.equal(columns[4].isEmpty(), true);
+        assert.equal(columns[5].getPositioning(), ColumnPositioning.Last);
     });
 });
 
