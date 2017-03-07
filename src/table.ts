005b4514649d6f6fc1f81b839f929f4bb38bf7c5
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2017-03-07 20:43:26 +0200

    Add table factory tests and fix validation issues.

--- src/table.ts
+++ src/table.ts
@@ -4,7 +4,7 @@ export class Table {
     private _columns: Column[] = [];
 
     constructor(rawRows: string[][]) {
-        if (!TableColumnCountValidator.isValidTable(rawRows))
+        if (!TableValidator.areValidRows(rawRows))
             throw "Invalid table: mismatching column counts.";
         this._generateColumns(rawRows);
     }
@@ -39,19 +39,44 @@ export class TableFactory {
         const lines = text.split(/\r\n|\r|\n/);
         const columns = 0;
 
-        let rows = lines
+        // split by separators to get a 2d array remove any empty lines
+        const rows = lines
             .map(l => l.split("|"))
-            .filter(arr => !(arr.length == 1 && arr[0] == "")) // remove any empty lines
-            .filter((v, i) => i != 1); // remove the separator line from second line
+            .filter(arr => !(arr.length == 1 && arr[0] == ""));
 
-        return TableColumnCountValidator.isValidTable(rows)
-            ? new Table(rows)
+        // remove the separator line from second line
+        const rowsWithoutSeparator = TableValidator.containsHeaderSeparator(rows)
+            ? rows.filter((v, i) => i != 1) // remove the separator line from second line
+            : null;
+
+        return TableValidator.areValidRows(rowsWithoutSeparator)
+            ? new Table(rowsWithoutSeparator)
             : null;
     }
 }
 
-class TableColumnCountValidator {
-    public static isValidTable(rawRows: string[][]): boolean {
-        return !!rawRows && rawRows.length > 0 && rawRows.every(r => r.length == rawRows[0].length)
+class TableValidator {
+    public static containsHeaderSeparator(rawRows: string[][]): boolean {
+        if (!rawRows || rawRows.length < 1 || !rawRows[1])
+            return false;
+        const secondRow = rawRows[1];
+        return secondRow.every((v, i) => this.isSeparator(v, i == 0 || i == secondRow.length - 1));
+    }
+
+    private static isSeparator(cellValue: string, isFirstOrLast: boolean): boolean {
+        if (cellValue.trim() == "-")
+            return true;
+        // If the first or last column is empty then it still can be a header containing separator, 
+        // for example table starting or ending with borders ("|") would produce this.
+        if (cellValue.trim() == "" && isFirstOrLast)
+            return true;
+        return false;
+    }
+
+    public static areValidRows(rawRows: string[][]): boolean {
+        return !!rawRows && 
+                rawRows.length > 1 && // at least two rows are required
+                rawRows[0].length > 1 && // at least two columns are required
+                rawRows.every(r => r.length == rawRows[0].length); // all rows of a column must match the length of the first row of that column
     }
 }
