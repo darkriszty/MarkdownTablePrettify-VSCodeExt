1ef22e04422068bdec2c272be64669c3d8cca387
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2017-03-23 20:36:13 +0200

    Add extension tests.

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -5,18 +5,25 @@
 
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
+    const _extensionName = "darkriszty.markdown-table-prettify";
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test("Extension exists", () => {
+        assert.ok(vscode.extensions.getExtension(_extensionName));
+    });
+
+    test("Extension gets activated", () => {
+         vscode.extensions.getExtension(_extensionName)
+            .activate()
+            .then((publicApi) => {
+                    assert.ok(true);
+                }, rejectReason => {
+                    assert.fail(0, 1, `Extension not activated. Reason: ${rejectReason}`, null);
+                }
+            );
     });
 });
