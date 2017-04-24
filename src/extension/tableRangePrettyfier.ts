14787a7c4cf3e94d924558684df7a1cb94ce83c3
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2017-04-24 17:16:01 +0300

    Allow formatting when a single table takes up the whole document.

--- src/extension/tableRangePrettyfier.ts
+++ src/extension/tableRangePrettyfier.ts
@@ -17,43 +17,25 @@ export class TableRangePrettyfier implements vscode.DocumentRangeFormattingEditP
         const result: vscode.TextEdit[] = [];
         const selection = document.getText(range);
 
-        // this extension only works on selected tables, not on the entire document
-        const isWholeDocumentFormatting = this.isWholeDocumentFormatting(document, range);
-        if (!isWholeDocumentFormatting) {
-            let message: string = null;
-            let table: ITable = null;
+        let message: string = null;
+        let table: ITable = null;
 
-            try {
-                table = this._tableFactory.create(selection);
-                if (table == null) {
-                    message = "Mismatching table column sizes.";
-                } else {
-                    const formattedTable = table.prettyPrint();
-                    result.push(new vscode.TextEdit(range, formattedTable));
-                }
-
+        try {
+            table = this._tableFactory.create(selection);
+            if (table == null) {
+                message = "Mismatching table column sizes.";
+            } else {
+                const formattedTable = table.prettyPrint();
+                result.push(new vscode.TextEdit(range, formattedTable));
             }
-            catch (ex) {
-                this._logger.logError(ex);
-                console.error("Error: \n\n" + ex);
-            }
-
-            if (!!message)
-                this._logger.logInfo(message);
+        } catch (ex) {
+            this._logger.logError(ex);
+            console.error("Error: \n\n" + ex);
         }
 
+        if (!!message)
+            this._logger.logInfo(message);
+
         return result;
-    }
-
-    private isWholeDocumentFormatting(document: vscode.TextDocument, range: vscode.Range): boolean {
-        if (document.lineCount < 1)
-            return true;
-
-        const zeroPosition = new vscode.Position(0, 0);
-        const documentEndPosition = document.lineAt(document.lineCount - 1).range.end;
-        if (range.start.isEqual(zeroPosition) && range.end.isEqual(documentEndPosition))
-            return true;
-
-        return false;
     }
 }
