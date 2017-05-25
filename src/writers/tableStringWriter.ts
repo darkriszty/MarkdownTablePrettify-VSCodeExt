8aff59c4a89572abdd27c10fac7a22050a858c54
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2017-05-25 10:55:21 +0300

    Add table string writer tests.

--- src/writers/tableStringWriter.ts
+++ src/writers/tableStringWriter.ts
@@ -2,6 +2,13 @@ import { TableViewModel } from "../viewModels/tableViewModel";
 
 export class TableStringWriter {
     public writeTable(table: TableViewModel): string {
+        /*
+            * thow error for null
+            * write the header in the first row
+            * write the separator in the second row
+            * write the rows in the next n rows
+            * after all but the last columns, the separator "|" must be written
+        */
         return "";
     }
 }
