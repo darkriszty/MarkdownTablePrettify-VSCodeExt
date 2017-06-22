f7db8034fc508017a9ece8a53a727780d46d90f8
Krisztian Daroczi <k.daroczi@netmatch.ro>
2017-06-22 15:50:19 +0300

    Update the table range prettyfier

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,16 +1,24 @@
 'use strict';
 import * as vscode from 'vscode';
 import { TableRangePrettyfier } from "./tableRangePrettyfier";
-import { TableFactory } from "../models/tableFactory";
 import { VsWindowLogger } from "../diagnostics/logger";
+import { TableFactory } from "../modelFactory/tableFactory";
+import { TableValidator } from "../modelFactory/tableValidator";
+import { TableViewModelBuilder } from "../viewModelBuilders/tableViewModelBuilder";
+import { TableStringWriter } from "../writers/tableStringWriter";
 
 // This method is called when the extension is activated.
 // The extension is activated the very first time the command is executed.
 export function activate(context: vscode.ExtensionContext): void {
     const MD_MODE: vscode.DocumentFilter = { language: "markdown", scheme: "file" };
 
+    const validator = new TableValidator();
     let disposable = vscode.languages.registerDocumentRangeFormattingEditProvider(
-        MD_MODE, new TableRangePrettyfier(new TableFactory(), new VsWindowLogger())
+        MD_MODE, new TableRangePrettyfier(
+            new TableFactory(validator),
+            new TableViewModelBuilder(validator),
+            new TableStringWriter(),
+            new VsWindowLogger())
     );
 
     context.subscriptions.push(disposable);
--- src/extension/tableRangePrettyfier.ts
+++ src/extension/tableRangePrettyfier.ts
@@ -1,12 +1,16 @@
 import * as vscode from "vscode";
-import { ITable } from "../models/table";
-import { ITableFactory } from "../models/tableFactory";
 import { ILogger } from "../diagnostics/logger";
+import { TableViewModel } from "../viewModels/tableViewModel";
+import { TableFactory } from "../modelFactory/tableFactory";
+import { TableViewModelBuilder } from "../viewModelBuilders/tableViewModelBuilder";
+import { TableStringWriter } from "../writers/tableStringWriter";
 
 export class TableRangePrettyfier implements vscode.DocumentRangeFormattingEditProvider {
 
     constructor(
-        private _tableFactory: ITableFactory,
+        private _tableFactory: TableFactory,
+        private _viewModelBuilder: TableViewModelBuilder,
+        private _writer: TableStringWriter,
         private _logger: ILogger
     ) { }
 
@@ -18,14 +22,13 @@ export class TableRangePrettyfier implements vscode.DocumentRangeFormattingEditP
         const selection = document.getText(range);
 
         let message: string = null;
-        let table: ITable = null;
-
         try {
-            table = this._tableFactory.create(selection);
-            if (table == null) {
+            const rows: string[][] = this._tableFactory.getModel(selection);
+            if (rows == null) {
                 message = "Mismatching table column sizes.";
             } else {
-                const formattedTable = table.prettyPrint();
+                const tableVm = this._viewModelBuilder.build(rows);
+                const formattedTable = this._writer.writeTable(tableVm);
                 result.push(new vscode.TextEdit(range, formattedTable));
             }
         } catch (ex) {
