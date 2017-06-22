e389f52d033fe38a3094fca04a1af5be0fed2e0c
Krisztian Daroczi <k.daroczi@netmatch.ro>
2017-06-22 15:34:40 +0300

    Remove boilerplate comments from test

--- test/extension/extension.test.ts
+++ test/extension/extension.test.ts
@@ -1,13 +1,6 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
     const _extensionName = "darkriszty.markdown-table-prettify";
 
