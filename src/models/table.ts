9992d980f100d2df0b2b21be048e1d6475b472fc
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2017-08-05 13:05:43 +0300

    Add a table model with tests

---
+++ src/models/table.ts
@@ -0,0 +1,43 @@
+export class Table {
+
+    constructor(private _rows: string[][]) { }
+
+    get items(): string[][] {
+        return this._rows;
+    }
+
+    public withoutEmptyColumns(): Table {
+        return new Table(this.removeEmptyColumns());
+    }
+
+    private removeEmptyColumns(): string[][] {
+        let emptyColumnIndexes: number[] = this.getEmptyColumnIndexes();
+
+        let clonedRows = this._rows.map(arr => arr.slice(0));
+        for (let i = emptyColumnIndexes.length - 1; i >=0; i--)
+            this.removeColumn(clonedRows, emptyColumnIndexes[i]);
+
+        return clonedRows;
+    }
+
+    private getEmptyColumnIndexes(): number[] {
+        let emptyColumnIndexes: number[] = [];
+        const colLength = this._rows[0].length;
+        for (let col = 0; col < colLength; col++)
+            if (this.isColumnEmpty(col))
+                emptyColumnIndexes.push(col);
+        return emptyColumnIndexes;
+    }
+
+    private isColumnEmpty(column: number): boolean {
+        for (let row = 0; row < this._rows.length; row++)
+            if (this._rows[row][column].trim() != "")
+                return false;
+        return true;
+    }
+
+    private removeColumn(rows: string[][], column: number): void {
+        for (let row = 0; row < rows.length; row++)
+            rows[row].splice(column, 1);
+    }
+}
---
+++ test/models/table.test.ts
@@ -0,0 +1,43 @@
+import * as assert from 'assert';
+import { Table } from "../../src/models/table";
+
+suite("Table tests", () => {
+
+    test("items() returns parameter given to constructor", () => {
+        const rows = [ 
+            [ "",   "  h1  ",   " " ,   "  h3  ", "" ],
+            [ "",   " - "   ,   ""  ,   "---"   , "" ],
+            [ "",   "c"     ,   "  ",   "e"     , "" ]
+        ];
+        const table = new Table(rows);
+
+        assert.equal(table.items, rows);
+    });
+
+    test("withoutEmptyColumns() create instance without empty columns and leaves original intact", () => {
+        const originalRows = [ 
+            [ "",   "  h1  ",   " " ,   "  h3  ", "" ],
+            [ "",   " - "   ,   ""  ,   "---"   , "" ],
+            [ "",   "c"     ,   "  ",   "e"     , "" ]
+        ];
+        const expectedNoEmptyColumns = [ 
+            [ "  h1  ", "  h3  " ],
+            [ " - "   , "---"    ],
+            [ "c"     , "e"      ]
+        ];
+
+        const table = new Table(originalRows);
+        const tableWithoutEmptyColumns = table.withoutEmptyColumns();
+
+        const actualNoEmptyColumnItems = tableWithoutEmptyColumns.items;
+        assert.equal(actualNoEmptyColumnItems.length, expectedNoEmptyColumns.length);
+        for (let i = 0; i < actualNoEmptyColumnItems.length; i++) {
+            assert.equal(actualNoEmptyColumnItems[i].length, expectedNoEmptyColumns[i].length);
+
+            for (let j = 0; j < actualNoEmptyColumnItems[i].length; j++)
+                assert.equal(actualNoEmptyColumnItems[i][j], expectedNoEmptyColumns[i][j]);
+        }
+
+        assert.equal(table.items, originalRows);
+    });
+});
