345c0f2e0674ff0811bebb209d9aea461ed1eead
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2018-02-02 09:29:50 +0200

    Use system EOL and reduce duplication in table writer

--- src/writers/tableStringWriter.ts
+++ src/writers/tableStringWriter.ts
@@ -1,4 +1,6 @@
+import { EOL } from "os";
 import { TableViewModel } from "../viewModels/tableViewModel";
+import { RowViewModel } from "../viewModels/rowViewModel";
 
 export class TableStringWriter {
     public writeTable(table: TableViewModel): string {
@@ -6,42 +8,33 @@ export class TableStringWriter {
         if (table.header == null) throw new Error("Table must have a header.");
         if (table.separator == null) throw new Error("Table must have a separator.");
         if (table.rows == null || table.rowCount == 0) throw new Error("Table must have rows.");
-        /*
-            * thow error for null
-            * write the header in the first row
-            * write the separator in the second row
-            * write the rows in the next n rows
-            * after all but the last columns, the separator "|" must be written
-            * in case the view model has left border, start all rows with separator
-            * in case the view model has right border, end all rows with separator
-        */
-        let buffer = this.getLeftBorderIfNeeded(table);
-        for (let col = 0; col < table.columnCount; col++) {
-            buffer += table.header.getValueAt(col);
-            buffer += this.getSeparatorIfNeeded(table, col);
-        }
-        buffer += this.getRightBorderIfNeeded(table);
-        buffer += "\r\n";
 
+        let buffer = "";
+        buffer += this.writeRowViewModel(table.header, table, true);
+        buffer += this.writeRowViewModel(table.separator, table, true);
+        buffer += this.writeRows(table);
+
+        return buffer;
+    }
+
+    private writeRows(table: TableViewModel): string {
+        let buffer = "";
+        for (let row = 0; row < table.rowCount; row++) {
+            buffer += this.writeRowViewModel(table.rows[row], table, row != table.rowCount - 1);
+        }
+        return buffer;
+    }
+
+    private writeRowViewModel(row: RowViewModel, table: TableViewModel, addEndOfLine: boolean): string {
+        let buffer = "";
         buffer += this.getLeftBorderIfNeeded(table);
         for (let col = 0; col < table.columnCount; col++) {
-            buffer += table.separator.getValueAt(col);
+            buffer += row.getValueAt(col);
             buffer += this.getSeparatorIfNeeded(table, col);
         }
         buffer += this.getRightBorderIfNeeded(table);
-        buffer += "\r\n";
-        
-        for (let row = 0; row < table.rowCount; row++) {
-            buffer += this.getLeftBorderIfNeeded(table);
-            for (let col = 0; col < table.columnCount; col++) {
-                buffer += table.rows[row].getValueAt(col);
-                buffer += this.getSeparatorIfNeeded(table, col);
-            }
-            buffer += this.getRightBorderIfNeeded(table);
-            if (row != table.rowCount - 1)
-                buffer += "\r\n";
-        }
-
+        if (addEndOfLine)
+            buffer += EOL;
         return buffer;
     }
 
