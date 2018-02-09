e94177416bcb3e857f3975ab2ccffceee4ca1448
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2018-02-09 17:34:46 +0200

    Use manual split to ignore escaped separators

--- src/modelFactory/selectionInterpreter.ts
+++ src/modelFactory/selectionInterpreter.ts
@@ -1,11 +1,28 @@
 export class SelectionInterpreter {
     public allRows(selection: string): string[][] {
         return selection.split(/\r\n|\r|\n/)
-            .map(l => l.split("|"))
-            .filter(arr => !(arr.length == 1 && /^\s*$/.test(arr[0])));
+            .map(this.splitLine, this)
+            .filter(arr => arr.length > 0 && !(arr.length == 1 && /^\s*$/.test(arr[0])));
     }
 
     public separator(selection: string): string[] {
         return this.allRows(selection).filter((v, i) => i == 1)[0];
+    }
+
+    private splitLine(line: string): string[] {
+        if (line == null || line.length == 0) return [];
+
+        let result:string[] = [],
+            index = -1,
+            previousSplitIndex = -1;
+        while ((index = line.indexOf("|", index + 1)) > -1) {
+            if (line[index - 1] != "\\") {
+                result.push(line.substring(previousSplitIndex + 1, index));
+                previousSplitIndex = index;
+            }
+        }
+        result.push(line.substring(previousSplitIndex + 1));
+
+        return result;
     }
 }
--- test/unitTests/modelFactory/selectionInterpreter.test.ts
+++ test/unitTests/modelFactory/selectionInterpreter.test.ts
@@ -44,8 +44,24 @@ suite("SelectionInterpreter tests", () => {
         assert.equal(rows[1][1], "v2");
     });
 
-    test("allRows() doesn't consider \| as separator ", () => {
-        const text = "h1|h2\|still h2\nv1|v2";
+    test("allRows() line starting and ending with pipe is correctly splitted", () => {
+        const text = "|h1|h2|h3|h4|";
+        const sut = createSut();
+
+        const rows = sut.allRows(text);
+
+        assert.equal(rows.length, 1);
+        assert.equal(rows[0].length, 6);
+        assert.equal(rows[0][0], "");
+        assert.equal(rows[0][1], "h1");
+        assert.equal(rows[0][2], "h2");
+        assert.equal(rows[0][3], "h3");
+        assert.equal(rows[0][4], "h4");
+        assert.equal(rows[0][5], "");
+    });
+
+    test("allRows() doesn't consider \\| as separator ", () => {
+        const text = "h1|h2\\|still\\|h2\nv1|v2";
         const sut = createSut();
 
         const rows = sut.allRows(text);
@@ -54,7 +70,7 @@ suite("SelectionInterpreter tests", () => {
         assert.equal(rows[0].length, 2);
         assert.equal(rows[1].length, 2);
         assert.equal(rows[0][0], "h1");
-        assert.equal(rows[0][1], "h2\|still h2");
+        assert.equal(rows[0][1], "h2\\|still\\|h2");
         assert.equal(rows[1][0], "v1");
         assert.equal(rows[1][1], "v2");
     });
