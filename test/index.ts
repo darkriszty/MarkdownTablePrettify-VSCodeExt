a19a99db2ff287bfa2cb82fbd578de0a4fcb8ffa
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2020-07-04 20:36:25 +0200

    Fix the extensionDevelopmentPath value

--- test/index.ts
+++ test/index.ts
@@ -4,16 +4,19 @@ import { runTests } from 'vscode-test';
 
 async function main() {
 	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../');
-
-		// The path to the extension test script
-		// Passed to --extensionTestsPath
+		// The paths are relative from the `/out/test` folder.
+		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 		const extensionTestsPath = path.resolve(__dirname, './suite');
 
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
+		await runTests({
+			extensionDevelopmentPath,
+			extensionTestsPath,
+			launchArgs: [
+				// This disables all extensions except the one being tested.
+				'--disable-extensions'
+			],
+		});
 	} catch (err) {
 		console.error('Failed to run tests');
 		process.exit(1);
