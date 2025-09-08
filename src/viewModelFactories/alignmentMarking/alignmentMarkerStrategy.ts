667be58c096076a49f127eb73487d6353627ddce
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2025-09-08 16:47:08 +0200

    Use cached instances in PadCalculatorSelector and AlignmentMarkerStrategy

--- src/viewModelFactories/alignmentMarking/alignmentMarkerStrategy.ts
+++ src/viewModelFactories/alignmentMarking/alignmentMarkerStrategy.ts
@@ -1,16 +1,25 @@
 import { Alignment } from "../../models/alignment";
 import { IAlignmentMarker, LeftAlignmentMarker, RightAlignmentMarker, CenterAlignmentMarker, NotSetAlignmentMarker } from ".";
 
-
 export class AlignmentMarkerStrategy {
-    constructor(private _markerChar: string) { }
+    private readonly _leftMarker: IAlignmentMarker;
+    private readonly _rightMarker: IAlignmentMarker;
+    private readonly _centerMarker: IAlignmentMarker;
+    private readonly _notSetMarker: IAlignmentMarker;
+
+    constructor(private _markerChar: string) {
+        this._leftMarker = new LeftAlignmentMarker(this._markerChar);
+        this._rightMarker = new RightAlignmentMarker(this._markerChar);
+        this._centerMarker = new CenterAlignmentMarker(this._markerChar);
+        this._notSetMarker = new NotSetAlignmentMarker();
+    }
 
     public markerFor(alignment: Alignment): IAlignmentMarker {
         switch (alignment) {
-            case Alignment.Left: return new LeftAlignmentMarker(this._markerChar);
-            case Alignment.Right: return new RightAlignmentMarker(this._markerChar);
-            case Alignment.Center: return new CenterAlignmentMarker(this._markerChar);
-            default: return new NotSetAlignmentMarker();
+            case Alignment.Left: return this._leftMarker;
+            case Alignment.Right: return this._rightMarker;
+            case Alignment.Center: return this._centerMarker;
+            default: return this._notSetMarker;
         }
     }
 }
